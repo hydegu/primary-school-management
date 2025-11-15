e60eeac407f55c450e0ab830f46038979a52fa35
java_hy <degulasihanyu@gmail.com>
2025-11-15 10:31:37 +0800

    first

---
+++ src/main/java/com/example/primaryschoolmanagement/PrimarySchoolManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.primaryschoolmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimarySchoolManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrimarySchoolManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/primaryschoolmanagement/PrimarySchoolManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.primaryschoolmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimarySchoolManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
